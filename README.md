d70ddaece975107c324423bf6c52b88eac6b60d0
Shane Muirhead <shane@studio116.co.uk>
2021-12-16 11:43:34 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aoc_2021 Solutions
+
+Hey everyone here are my aoc 2021 solutions in Python
---
+++ puzzle_one.py
@@ -0,0 +1,40 @@
+from aocd.models import Puzzle
+from aocd import submit
+day_1 = Puzzle(year=2021, day=1)
+day_2 = Puzzle(year=2021, day=2)
+my_data = day_1.input_data
+my_data_day_2 = day_2.input_data
+
+def puzzle_one():
+	""" algorithm for puzzle one part 1 aoc"""
+	numbers = []
+	counter = 0
+
+	for line in my_data.splitlines():
+		numbers.append(int(line))
+
+	first_num = numbers[0]
+
+	for n in numbers[1:]:
+		if n > first_num:
+			counter += 1
+		first_num = n
+
+	submit(counter, part="a", day=1, year=2021) # Correct ONTO part 2
+
+def puzzle_one_part_two():
+	numbers = []
+	counter = 0
+
+	for line in my_data.splitlines():
+		numbers.append(int(line))
+
+	count = sum(
+		numbers[i] > numbers[i - 3]
+		for i in range(1, len(numbers))
+		)
+
+	submit(count, part="b", day=1, year=2021) # Correct ONTO puzzle 2
+
+print(my_data_day_2)
+
